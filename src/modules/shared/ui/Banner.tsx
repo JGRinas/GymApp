30f66e5f38d5b6d3f38380e166667ec72b9456e4
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-11 20:37:40 -0300

    added Banner component

---
+++ src/modules/shared/ui/Banner.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { colors } from "@colors/theme/colors";
+import { BaseText } from "./Texts";
+
+interface IBanner {
+  text: string;
+  variant?: "first" | "second";
+  icon?: React.ReactNode;
+}
+
+export const Banner = ({ text, variant = "first", icon }: IBanner) => {
+  return (
+    <View style={[styles.banner, styles[variant]]}>
+      {icon && <View style={styles.icon}>{icon}</View>}
+      <BaseText textStyles={styles.text}>{text}</BaseText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  banner: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 5,
+  },
+  first: {
+    backgroundColor: colors.PRIMARY,
+  },
+  second: {
+    backgroundColor: colors.TERTIARY,
+  },
+  text: {
+    fontSize: 16,
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
