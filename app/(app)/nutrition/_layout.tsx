bc8ca5b7f9149b106a15cc307b4e5bdfef2b8f93
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-14 01:26:42 -0300

    added nutrition screen

--- app/(app)/nutrition/_layout.tsx
+++ app/(app)/nutrition/_layout.tsx
@@ -1,3 +1,6 @@
-export default function HomeLayout() {
-  return <></>;
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function NutritionLayout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
---
+++ src/modules/routines/infrastructure/nutrition-mock.ts
@@ -0,0 +1,28 @@
+const nutritionSteps = [
+  {
+    id: 1,
+    title: "Mitad del Plato: Verduras",
+    description:
+      "Se recomienda que la mitad del plato esté compuesto por verduras, preferentemente crudas o ligeramente cocidas. Las verduras aportan vitaminas, minerales, fibra y antioxidantes esenciales para el buen funcionamiento del cuerpo. \n\nEjemplos: Lechuga, espinaca, tomate, cebolla, pepino, zanahoria, brócoli.",
+  },
+  {
+    id: 2,
+    title: "Un Cuarto del Plato: Alimentos Ricos en Proteínas",
+    description:
+      "Un 25% del plato debe estar compuesto por proteínas saludables, esenciales para la construcción y reparación de tejidos. \n\nEjemplos: Pollo a la parrilla, pescado, huevos, legumbres (como garbanzos o lentejas) y tofu.",
+  },
+  {
+    id: 3,
+    title: "Un Cuarto del Plato: Cereales, Legumbres y Féculas",
+    description:
+      "El 25% restante del plato debe incluir carbohidratos complejos que proporcionan energía sostenida. \n\nEjemplos: Arroz integral, quinoa, avena, batata, papa y legumbres como lentejas.",
+  },
+  {
+    id: 4,
+    title: "Recomendación General: Hidratación y Moderación",
+    description:
+      "Acompaña tus comidas con agua para mantener una correcta hidratación. Evita el exceso de azúcares y grasas saturadas para mantener un estilo de vida saludable. Cambia los ingredientes con frecuencia para obtener una amplia gama de nutrientes.",
+  },
+];
+
+export default nutritionSteps;
