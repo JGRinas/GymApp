e0e9a77fb2d58187c9ab05bfc2e55da61a20a863
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-13 22:14:35 -0300

    create home screen

--- app/(app)/home/index.tsx
+++ app/(app)/home/index.tsx
@@ -1,11 +1,39 @@
-import { Text, View } from "react-native";
+import BackgroundImage from "@shared/ui/BackgroundImage";
+import { Banner } from "@shared/ui/Banner";
+import PlanCard from "@shared/ui/PlanCard";
+import WelcomeCard from "@shared/ui/WelcomeCard";
+import { StyleSheet, View } from "react-native";
+import { openWhatsApp } from "../../../src/modules/home/infrastructure/whatsapp-btn";
 
 function WelcomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <BackgroundImage source="Background2">
+      <View style={styles.container}>
+        <WelcomeCard />
+        <PlanCard />
+        <View style={styles.banner}>
+          <Banner
+            icon={"WhatsAppIcon"}
+            text="CONSULTAR POR WHATSAPP"
+            onPress={openWhatsApp}
+          />
+          <Banner text="ACCEDÉ A TU RUTINA" variant="second" />
+        </View>
+      </View>
+    </BackgroundImage>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    marginTop: 25,
+    gap: 15,
+  },
+  banner: {
+    gap: 15,
+  },
+});
 
 export default WelcomeScreen;
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
+import { useAppSelector } from "@config/store";
 import { Redirect } from "expo-router";
 
 function Index() {
+  const isLogged = useAppSelector((state) => state.AuthSlice.isLogged);
+
+  if (isLogged) return <Redirect href="/(app)/home" />;
   return <Redirect href="/(on-boarding)" />;
 }
 
