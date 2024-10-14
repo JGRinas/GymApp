0f3c262c94dbecac73fcd32586b3647a782fb5c5
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-13 23:23:32 -0300

    added profile screen

--- app/(app)/profile/index.tsx
+++ app/(app)/profile/index.tsx
@@ -1,5 +1,43 @@
+import BackgroundImage from "@shared/ui/BackgroundImage";
+import { Banner } from "@shared/ui/Banner";
+import PlanCard from "@shared/ui/PlanCard";
+import WelcomeCard from "@shared/ui/WelcomeCard";
+import { StyleSheet, View } from "react-native";
+import { openWhatsApp } from "../../../src/modules/home/infrastructure/whatsapp-btn";
+import { useRouter } from "expo-router";
+
 function ProfileScreen() {
-  return <></>;
+  const navigation = useRouter();
+  return (
+    <BackgroundImage source="Background2">
+      <View style={styles.container}>
+        <WelcomeCard
+          inHome={false}
+          onPressImage={() => navigation.push("/(app)/profile/edit-photo")}
+        />
+        <PlanCard />
+        <View style={styles.banner}>
+          <Banner
+            icon={"WhatsAppIcon"}
+            text="ENVIAR ALERTA POR WHATSAPP"
+            onPress={() => openWhatsApp("Activar Alerta")}
+          />
+        </View>
+      </View>
+    </BackgroundImage>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    marginTop: 25,
+    gap: 15,
+  },
+  banner: {
+    gap: 15,
+  },
+});
 
 export default ProfileScreen;
